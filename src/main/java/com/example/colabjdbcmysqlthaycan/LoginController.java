590de110c1782c96ffe6b8bdba3cb4ce174fa364
sheep <samcoxe0307@gmail.com>
2024-10-21 16:34:34 +0700

    >SD>F>SDF

--- src/main/java/com/example/colabjdbcmysqlthaycan/LoginController.java
+++ src/main/java/com/example/colabjdbcmysqlthaycan/LoginController.java
@@ -1,19 +1,6 @@
 package com.example.colabjdbcmysqlthaycan;
 
 import javafx.fxml.FXML;
-<<<<<<< HEAD
-import javafx.scene.control.Label;
-
-public class LoginController {
-    @FXML
-    private Label welcomeText;
-
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
-}
-=======
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -95,4 +82,4 @@ public class LoginController {
         }
     }
 }
->>>>>>> 2c7ee3576e99ddffb850ec49720f441f6ef5f314
+
