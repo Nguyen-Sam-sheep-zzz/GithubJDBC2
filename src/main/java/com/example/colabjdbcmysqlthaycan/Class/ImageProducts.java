e3ab8f5bdb93798770d566036799e2e396d3066b
sheep <samcoxe0307@gmail.com>
2024-10-24 08:37:46 +0700

    Images

--- src/main/java/com/example/colabjdbcmysqlthaycan/Class/ImageProducts.java
+++ src/main/java/com/example/colabjdbcmysqlthaycan/Class/ImageProducts.java
@@ -3,16 +3,14 @@ package com.example.colabjdbcmysqlthaycan.Class;
 public class ImageProducts {
     private int idProduct;
     private int idImage;
-    private int quantity;
 
     public ImageProducts() {
 
     }
 
-    public ImageProducts(int idProduct, int idImage, int quantity) {
+    public ImageProducts(int idProduct, int idImage) {
         this.idProduct = idProduct;
         this.idImage = idImage;
-        this.quantity = quantity;
     }
 
     public int getIdProduct() {
@@ -31,20 +29,11 @@ public class ImageProducts {
         this.idImage = idImage;
     }
 
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
     @Override
     public String toString() {
         return "ImageProducts{" +
                 "idProduct=" + idProduct +
                 ", idImage=" + idImage +
-                ", quantity=" + quantity +
                 '}';
     }
 }
