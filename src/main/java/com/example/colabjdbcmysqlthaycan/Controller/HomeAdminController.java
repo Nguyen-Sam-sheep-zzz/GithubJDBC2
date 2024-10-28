5a1d4b14a6f36fbe4337b37060ab8a212ea2f4eb
kieutandung <tandungkieu1312@gmail.com>
2024-10-28 08:45:53 +0700

    beta

--- src/main/java/com/example/colabjdbcmysqlthaycan/Controller/HomeAdminController.java
+++ src/main/java/com/example/colabjdbcmysqlthaycan/Controller/HomeAdminController.java
@@ -19,32 +19,24 @@ import javafx.stage.Stage;
 import java.io.File;
 import java.sql.*;
 
+
+
 public class HomeAdminController {
-
-    private final ConnectDB connectDB = new ConnectDB();
-
-
+    private ConnectDB connectDB = new ConnectDB();
     @FXML
     private TableView<ProductDisplay> productTableView;
-
     @FXML
     private TableColumn<ProductDisplay, String> imageColumn;
-
     @FXML
     private TableColumn<ProductDisplay, Integer> idColumn;
-
     @FXML
     private TableColumn<ProductDisplay, String> nameColumn;
-
     @FXML
     private TableColumn<ProductDisplay, String> descriptionColumn;
-
     @FXML
     private TableColumn<ProductDisplay, Double> priceColumn;
-
     @FXML
     private TableColumn<ProductDisplay, String> statusColumn;
-
     @FXML
     private TextField nameLaptopTextField;
     @FXML
@@ -53,7 +45,6 @@ public class HomeAdminController {
     private ComboBox<String> statusLaptopTextField;
     @FXML
     private TextField priceLaptopTextField;
-
     @FXML
     private ImageView imageLaptopImageView;
 
@@ -64,7 +55,6 @@ public class HomeAdminController {
         descriptionColumn.setCellValueFactory(new PropertyValueFactory<>("description"));
         priceColumn.setCellValueFactory(new PropertyValueFactory<>("price"));
         statusColumn.setCellValueFactory(new PropertyValueFactory<>("status"));
-
         productTableView.setItems(getProductDisplayList());
     }
 
@@ -74,11 +64,9 @@ public class HomeAdminController {
                 "FROM Products p " +
                 "JOIN ImageProducts ip ON p.idProduct = ip.idProduct " +
                 "JOIN Images i ON ip.idImage = i.idImage";
-
         try (Connection connection = connectDB.connectionDB();
              PreparedStatement preparedStatement = connection.prepareStatement(query);
              ResultSet resultSet = preparedStatement.executeQuery()) {
-
             while (resultSet.next()) {
                 int id = resultSet.getInt("idProduct");
                 String name = resultSet.getString("nameProduct");
@@ -89,7 +77,6 @@ public class HomeAdminController {
 
                 productDisplayList.add(new ProductDisplay(imageLink, id, name, description, price, status));
             }
-
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -105,54 +92,56 @@ public class HomeAdminController {
         File file = fileChooser.showOpenDialog(new Stage());
         if (file != null) {
             Image image = new Image(file.toURI().toString());
-            if (imageLaptopImageView != null) {
-                imageLaptopImageView.setImage(image);
-            }
+            imageLaptopImageView.setImage(image);
         }
     }
 
     public void addProduct(ActionEvent actionEvent) {
-
         String queryProduct = "INSERT INTO Products (nameProduct, productDescription, price, status) VALUES (?, ?, ?, ?)";
         String queryImage = "INSERT INTO Images (link) VALUES (?)";
         String queryImageProduct = "INSERT INTO ImageProducts (idImage, idProduct) VALUES (?, ?)";
 
-        try (Connection connection = connectDB.connectionDB();
-             PreparedStatement psProduct = connection.prepareStatement(queryProduct, Statement.RETURN_GENERATED_KEYS);
-             PreparedStatement psImage = connection.prepareStatement(queryImage, Statement.RETURN_GENERATED_KEYS);
-             PreparedStatement psImageProduct = connection.prepareStatement(queryImageProduct)) {
-
+        try {
+            PreparedStatement psProduct = connectDB.connectionDB().prepareStatement(queryProduct, Statement.RETURN_GENERATED_KEYS);
             psProduct.setString(1, nameLaptopTextField.getText());
             psProduct.setString(2, descriptionLaptopTextField.getText());
             psProduct.setDouble(3, Double.parseDouble(priceLaptopTextField.getText()));
-            psProduct.setString(4, statusLaptopTextField.getValue() != null ? statusLaptopTextField.getValue() : ""); // Kiểm tra null
+            psProduct.setString(4, statusLaptopTextField.getValue());
             psProduct.executeUpdate();
-
             ResultSet generatedKeys = psProduct.getGeneratedKeys();
-            long idProduct = 0;
+            int productId = 0;
             if (generatedKeys.next()) {
-                idProduct = generatedKeys.getLong(1);
+                productId = generatedKeys.getInt(1);
+            }
+            String imagePath = imageLaptopImageView.getImage().getUrl();
+            int lastSlashIndex = imagePath.lastIndexOf("/");
+            int imageId;
+            if (lastSlashIndex != -1) {
+                String imageName = imagePath.substring(lastSlashIndex + 1);
+                PreparedStatement psImage = connectDB.connectionDB().prepareStatement(queryImage, Statement.RETURN_GENERATED_KEYS);
+                psImage.setString(1, imageName);
+                psImage.executeUpdate();
+                ResultSet imageKeys = psImage.getGeneratedKeys();
+                imageId = 0;
+                if (imageKeys.next()) {
+                    imageId = imageKeys.getInt(1);
+                }
+            } else {
+                throw new RuntimeException("Invalid image path: " + imagePath);
             }
 
-            psImage.setString(1, imageLaptopImageView.getImage().getUrl());
-            psImage.executeUpdate();
-
-            ResultSet generatedImageKeys = psImage.getGeneratedKeys();
-            long idImage = 0;
-            if (generatedImageKeys.next()) {
-                idImage = generatedImageKeys.getLong(1);
-            }
-
-            psImageProduct.setLong(1, idImage);
-            psImageProduct.setLong(2, idProduct);
+            PreparedStatement psImageProduct = connectDB.connectionDB().prepareStatement(queryImageProduct);
+            psImageProduct.setInt(1, imageId);
+            psImageProduct.setInt(2, productId);
             psImageProduct.executeUpdate();
+            System.out.println("Product added successfully");
 
-        } catch (SQLException e) {
+            productTableView.setItems(getProductDisplayList());
+
+        } catch (SQLException | NumberFormatException e) {
             e.printStackTrace();
             throw new RuntimeException("Add product failed.", e);
-        } catch (NumberFormatException e) {
-            e.printStackTrace();
-            throw new RuntimeException("Price must be a valid number.", e);
         }
     }
+
 }
